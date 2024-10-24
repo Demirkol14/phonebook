502d4bd11548f6934a830cf442a9c12a215a4164
Demirkol14 <mdemirkol14@icloud.com>
2024-10-24 23:27:22 +0200

    Add files via upload
    project added

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "Demirkol14"
+}
+
+variable "git-token" {
+  default = "***********"
+}
+
+variable "key-name" {
+  default = "firstkey"
+}
+
+variable "hosted-zone" {
+  default = "devopsdemirkol.com"
+}
